7cd56762bbd70d1a28bc22354b64b6d3f13b632c
tu-leminh <mt.the.dev@gmail.com>
2025-04-23 21:56:16 +0700

    fix minuet

--- lua/plugins/minuet.lua
+++ lua/plugins/minuet.lua
@@ -3,22 +3,26 @@ return {
     "milanglacier/minuet-ai.nvim",
     dependencies = {
       "nvim-lua/plenary.nvim",
-      "hrsh7th/nvim-cmp",
-      "Saghen/blink.cmp"
     },
     config = function()
       require('minuet').setup {
+        virtualtext = {
+          auto_trigger_ft = {"*"},
+          keymap = {
+            accept = '<A-A>',
+            accept_line = '<A-a>',
+            accept_n_lines = '<A-z>',
+            prev = '<A-[>',
+            next = '<A-]>',
+            dismiss = '<A-e>',
+          },
+        },
         provider = 'openai_fim_compatible',
-        n_completions = 3, -- recommend for local model for resource saving
-        -- I recommend beginning with a small context window size and incrementally
-        -- expanding it, depending on your local computing power. A context window
-        -- of 512, serves as an good starting point to estimate your computing
-        -- power. Once you have a reliable estimate of your local computing power,
-        -- you should adjust the context window to a larger value.
-        context_window = 512,
+        throttle = 5000,
+        notify = false,
         provider_options = {
           openai_fim_compatible = {
-            api_key = "PATH",
+            api_key = 'PATH',
             name = 'Ollama',
             end_point = 'http://localhost:11434/v1/completions',
             model = 'qwen2.5-coder:7b',
